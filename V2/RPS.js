3b10daaac7f977aae67591d2b9e48cfe444b1be6
cpree <caldonpreece@gmail.com>
2022-08-01 23:22:07 -0600

    added play game function and score.

--- V2/RPS.js
+++ V2/RPS.js
@@ -1,8 +1,66 @@
-let choices = ['Rock','Paper','Scissors']
-let playerSelection = prompt('Choose Your Weapon. (Rock, Paper or Scissors)')
+let choices = ['rock','paper','scissors']
+let score = 0
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+function playerInput() {
+  playerChoice = prompt('Choose Your Weapon. (Rock, Paper or Scissors)').toLowerCase()
+  if (choices.includes(playerChoice)) {
+    return playerChoice
+  } else {
+   return playerInput(); 
+  }
+};
 
 function getComputerChoice() {
   return choices[Math.floor(Math.random()*choices.length)]
 };
 
-playRound(playerSelection, computerSelection)
+
+function youWin(player, computer) {
+  score += 1
+  alert("You Win : " + capitalizeFirstLetter(player) + " > " + capitalizeFirstLetter(computer))
+};
+function youLose(player, computer) {
+  alert("You Lose : " + capitalizeFirstLetter(player) + " < " + capitalizeFirstLetter(computer))
+};
+function youTie(player, computer) {
+  alert("You Tie : " + capitalizeFirstLetter(player) + " = " + capitalizeFirstLetter(computer))
+  playRound(playerInput(), getComputerChoice())
+};
+
+function playRound(playerSelection, computerSelection) {
+  console.log('player = ' + playerSelection)
+  console.log('computer = ' + computerSelection)
+  if (computerSelection === playerSelection) {
+    return youTie (playerSelection, computerSelection);
+  } else if (playerSelection === 'paper') {
+    if (computerSelection === 'rock') {
+      return youWin(playerSelection, computerSelection);
+    } else {
+      return youLose(playerSelection, computerSelection);
+    }
+  } else if (playerSelection === 'rock') {
+    if (computerSelection === 'scissors') {
+      youWin(playerSelection, computerSelection);
+    } else {
+      return youLose(playerSelection, computerSelection);
+    }
+  }  else if (playerSelection === 'scissors') {
+    if (computerSelection === 'paper') {
+      return youWin(playerSelection, computerSelection);
+    } else {
+      return youLose(playerSelection, computerSelection);
+    }
+  }
+};
+
+function playGame() {
+  score = 0 
+  for (let i = 0; i < 5; i++) {
+    playRound(playerInput(), getComputerChoice());
+    console.log('You\'ve won ' + score + ' out of ' + (i+1) + ' games')
+  }
+};
