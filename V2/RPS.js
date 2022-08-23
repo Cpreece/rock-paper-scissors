f29d9fb66fd48a65adce7a7122aee3b3bf868bf0
cpree <caldonpreece@gmail.com>
2022-08-23 00:02:25 -0600

    added UI to game

--- V2/RPS.js
+++ V2/RPS.js
@@ -1,43 +1,55 @@
 let choices = ['rock','paper','scissors'];
-let playerScore, computerScore
+let playerScore = 0
+let computerScore = 0
+let nodes = document.querySelectorAll('.choice')
 
+//support functions 
 function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 };
 
-function playerInput() {
-  playerChoice = prompt('Choose Your Weapon. (Rock, Paper or Scissors)').toLowerCase()
-  if (choices.includes(playerChoice)) {
-    return playerChoice;
-  } else {
-   return playerInput(); 
-  }
-};
+// game mechanics
 
 function getComputerChoice() {
   return choices[Math.floor(Math.random()*choices.length)];
 };
 
+function playerInput(e) {
+  playerChoice = this.id
+  if (playerScore + computerScore < 5) {
+    playRound(playerChoice, getComputerChoice())
+  } else {
+    document.querySelector('.reset').classList.toggle('inactive');
+    document.querySelector('.choices-inner').classList.toggle('inactive');
+    if (playerScore > computerScore) {
+      document.querySelector('.instructions').textContent = 'Congratulations. You\'ve beaten a helpless machine.'
+    } else {
+      document.querySelector('.instructions').textContent = 'By losing you have become meat for the machine.'
+    };
+  };
+  e.stopPropagation();
+};
+  
 
 function youWin(player, computer) {
-  playerScore += 1
-  alert("You Win : " + capitalizeFirstLetter(player) + " > " 
-    + capitalizeFirstLetter(computer));
+  playerScore += 1;
+  document.querySelector('.instructions').textContent = "You Win : " + capitalizeFirstLetter(player) + " > " 
+  + capitalizeFirstLetter(computer);
+  document.querySelector('#human > .score').textContent = playerScore
 };
 function youLose(player, computer) {
-  computerScore += 1
-  alert("You Lose : " + capitalizeFirstLetter(player) + " < " 
-    + capitalizeFirstLetter(computer));
+  computerScore += 1;
+  document.querySelector('.instructions').textContent = "You Lose : " + capitalizeFirstLetter(player) + " < " 
+    + capitalizeFirstLetter(computer);
+    document.querySelector('#computer > .score').textContent = computerScore
+
 };
 function youTie(player, computer) {
-  alert("You Tie : " + capitalizeFirstLetter(player) + " = " 
-    + capitalizeFirstLetter(computer));
-  playRound(playerInput(), getComputerChoice());
+  document.querySelector('.instructions').textContent = "You Tie : " + capitalizeFirstLetter(player) + " = " 
+    + capitalizeFirstLetter(computer);
 };
 
 function playRound(playerSelection, computerSelection) {
-  console.log('player = ' + playerSelection);
-  console.log('computer = ' + computerSelection)
   if (computerSelection === playerSelection) {
     return youTie (playerSelection, computerSelection);
   } else if (playerSelection === 'paper') {
@@ -61,16 +73,22 @@ function playRound(playerSelection, computerSelection) {
   }
 };
 
-function playGame() {
-  playerScore = 0 
+function newGame() {
+  document.querySelector('.reset').classList.toggle('inactive');
+  document.querySelector('.choices-inner').classList.toggle('inactive');
+  playerScore = 0
   computerScore = 0
-  for (let i = 0; i < 5; i++) {
-    playRound(playerInput(), getComputerChoice());
-    console.log('You\'ve won ' + playerScore + ' out of ' + (i+1) + ' games')
-  }
-  if (playerScore > computerScore) {
-    alert('Congratulations. You\'ve beaten a helpless machine.')
-  } else {
-    alert('By losing you have become meat for the machine.')
-  }
-};
+  document.querySelector('#human > .score').textContent = playerScore
+  document.querySelector('#computer > .score').textContent = computerScore
+}
+
+// event listeners
+
+nodes.forEach(item => { item.addEventListener('click', playerInput)
+});
+
+document.querySelector('.reset > button').addEventListener('click', newGame)
+
+
+
+
